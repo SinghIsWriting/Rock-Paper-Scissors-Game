5a63b823ee778faf075408fcaf6ff236b4368dfc
Singh <sabhisheksignh343104@gmail.com>
2023-06-11 17:27:47 +0530

    Added comments in the code

--- main.c
+++ main.c
@@ -1,40 +1,28 @@
-/*
-
-Create Rock, paper, scissors game
-Player 1: rock
-Player 2: (computer): scissors --> player 1 gets 1 point
-
-Rock beats scissors: Rock crushes or breaks scissors.
-Scissors beats paper: Scissors cut or shred paper.
-Paper beats rock: Paper covers or wraps rock.
-
-Write a C program to allow user to play this game three times with computer. Log the scores of computer and the player. Display the name of the winner at the end
-
-Notes: You have to display name of the player during the game. Take user's name as an input from the user.
-
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdlib.h>     // for srand()
+#include <time.h>       // for time()
 
+/* This function creates a random number between 0 and given num */
 int generate_rand_num(int num)
 {
-    /* This function creates a random number between 0 and given num */
-    srand(time(NULL)); // srand takes seed as an input and is defined inside stdlib.h
+    srand(time(NULL));         // srand takes seed as an input and is defined inside stdlib.h
     return rand() % num;
 }
 
 int main()
 {
     printf("\n\t************ Welcome to Rock-Paper-Scissors Game ************\n\n");
+    // Creating array of characters for options to choosen by computer
     char computer[] = {'r', 'p', 's'};
+
     char username[20], user_input;
     int user_score = 0, computer_score = 0;
     printf("Please enter your name to start the game: ");
     scanf("%s", &username);
     getchar();
     int a = 0;
+
+    // Implementing a while loop to run the game continuously untill the user choose to quit the game
     while (1)
     {
         printf("\nChoose one of the Rock(r), Paper(p), Scissors(s) or Quit(q)\n");
@@ -91,8 +79,9 @@ int main()
             }
         }
         getchar();
-        a++;
+        a++;         // Counting how many times game is played between computer and user
     }
+
     printf("\n\n\t**************** GAME OVER ****************\n");
     printf("\nComputer's score: %d/%d", computer_score, a);
     printf("\n%s's score: %d/%d\n\n", username, user_score, a);
